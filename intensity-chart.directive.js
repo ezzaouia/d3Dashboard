4c9f022f96ac1f6527b8f7171dc1ef17c19dff1c
Mohamed Ezzaouia <mohamed@speakplus.fr>
2016-08-07 13:16:34 +0200

    adding legend

--- intensity-chart.directive.js
+++ intensity-chart.directive.js
@@ -25,8 +25,8 @@
         let positiveColorMap = colorMapInstance('positive');
         let negativeColorMap = colorMapInstance('negative');
 
-        let w = 100;
-        let h = 100;
+        let w = 200;
+        let h = 150;
         let barPadding = 1;
 
         let svg = d3UtilsService.d3
@@ -68,6 +68,65 @@
                         return negativeColorMap(d.value);
                 }
             });
+
+        // legend
+
+        let legendWidth = w * .7;
+        function addGradientLegend(intensityChartOptions) {
+            _.forEach(intensityChartOptions, function (object, index) {
+
+                let y = 70 + (index * 8);
+
+                var legend = svg.append("defs")
+                    .append("svg:linearGradient")
+                    .attr("id", "gradient-" + object.key)
+                    .attr("x1", "0%")
+                    .attr("y1", "0%")
+                    .attr("x2", "100%")
+                    .attr("y2", "0%")
+                    .attr("spreadMethod", "pad");
+
+                legend.append("stop")
+                    .attr("offset", "0%")
+                    .attr("stop-color", object.color0);
+
+                legend.append("stop")
+                    .attr("offset", "100%")
+                    .attr("stop-color", object.color1);
+
+                svg.append("rect")
+                    .attr("width", legendWidth)
+                    .attr("height", 7)
+                    .attr('y', y)
+                    .attr('x', 15)
+                    .style("fill", "url(#gradient-" + object.key + ')')
+
+                svg.append('text')
+                    .attr("class", "label")
+                    .attr('y', y + 7)
+                    .attr('x', w - 45)
+                    .text(object.key);
+
+            });
+        }
+
+        addGradientLegend(intensityChartCtrl.options);
+
+        var x = d3.scale.linear()
+            .range([0, legendWidth])
+            .domain([0, 100]);
+
+        var yAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(15, 94)")
+            .call(yAxis).append("text")
+            .attr("transform", "rotate(+0)")
+            .attr("y", 20).attr("dy", ".71em")
+            .attr("x", 40).attr("dx", ".71em")
+            .style("text-anchor", "center").text("Intensity scale");
+
 
         // some helpers methods
         function colorMapInstance(key) {
